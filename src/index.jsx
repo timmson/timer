7d6670ce38c0edcb96547eb6dff2d684de7aa6de
timmson <timmson666@mail.ru>
2022-01-09 02:16:07 +0300

    Merge to master and move old version to sub-folder #15

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 
 const Timer = require("./timer.jsx");
-const variants = ["01:00", "02:00", "03:00", "04:00", "05:00", "07:00", "10:00", "15:00", "20:00", "25:00"/*, "30:00", "45:00"*/];
+const variants = ["01:00", "02:00", "03:00", "04:00", "05:00", "07:00", "10:00", "15:00", "20:00", "25:00", "30:00", "45:00"];
 
 ReactDOM.render(<Timer audio={new Audio("beep.wav")} variants={variants}/>, document.getElementById("app"));
