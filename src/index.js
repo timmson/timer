a6df071e916b3b23e9d49ec9d1e4c4d332eb109c
timmson <timmson666@mail.ru>
2022-02-09 00:09:04 +0300

    Add PWA

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
 window.addEventListener("load", async () => {
 	if ("serviceWorker" in navigator) {
 		try {
-			await navigator.serviceWorker.register("sw.js");
+			await navigator.serviceWorker.register("./sw.js");
 		} catch (e) {
 			console.log("Service worker register fail");
 		}
